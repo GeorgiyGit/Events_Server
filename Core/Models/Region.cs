09b43fb630f5e8f43526cd277b2f543ef995ba1b
GeorgiyGit <102670724+GeorgiyGit@users.noreply.github.com>
2023-04-13 13:32:03 +0300

    add region

--- Core/Models/Region.cs
+++ Core/Models/Region.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Core.Models
 {
-	internal class Region
+	public class Region
 	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public Country Country { get; set; }
+		public int CountryId { get; set; }
+
+		public ICollection<City> Cities { get; set; } = new HashSet<City>();
 	}
 }
